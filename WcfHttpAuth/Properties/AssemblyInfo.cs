edc0f56b5195c88291db87e301fbf502fc066a72
javicrespo <javiercrespoalvez@gmail.com>
2010-09-25 10:54:55 +0200

    Setting version number

--- WcfHttpAuth/Properties/AssemblyInfo.cs
+++ WcfHttpAuth/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WcfWsse")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("WcfHttpAuth")]
+[assembly: AssemblyDescription("HTTP authentication capabilities for WCF REST services")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WcfWsse")]
+[assembly: AssemblyProduct("WcfHttpAuth")]
 [assembly: AssemblyCopyright("Copyright © 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: InternalsVisibleTo("WcfHttpAuth.IntegrationTests")]
